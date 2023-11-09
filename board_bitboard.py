9e1c0051ca1e12d26e168ed4ed0598be0964187b
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2023-11-09 22:20:11 +0100

    working base implementation

--- board_bitboard.py
+++ board_bitboard.py
@@ -9,6 +9,8 @@ NO_PIECES = 0x0
 VALID_MOVE_BB = 0x3FFFFFE
 VALID_MOVE_LEFT_1_BB = 0x1EF7BDE
 VALID_MOVE_RIGHT_1_BB = 0x3DEF7BC
+VALID_MOVE_LEFT_2_BB = 0xE739CE
+VALID_MOVE_RIGHT_2_BB = 0x39CE738
 
 BOARD_SIZE_X = 5
 BOARD_SIZE_Y = 5
@@ -62,7 +64,7 @@ def _win(b: Board) -> bool:
 def _tie(b: Board) -> bool:
     """Return True if the game is tied on board b, otherwise False.
     Precondition: not _win(b)"""
-    return False  # TODO
+    return not legal_moves(b)  # TODO: optimize
 
 
 def _index_can_move_diagonally(index: int) -> bool:
@@ -97,75 +99,7 @@ def black(b: Board) -> list[int]:
 
 def is_legal(m: Move, b: Board) -> bool:
     """Return True if move m is legal on board b, otherwise False."""
-    is_white = white_plays(b)
-    ally_bb = b.white_bb if is_white else b.black_bb
-    enemy_bb = b.black_bb if is_white else b.white_bb
-    source_index = source(m)
-
-    # Check if the source piece is empty
-    if not _get_bit(ally_bb, source_index):
-        return False
-
-    common_bb = ally_bb | enemy_bb
-    target_index = target(m)
-
-    # Check if the target piece is occupied
-    if _get_bit(common_bb, target_index):
-        return False
-
-    source_bb = _set_bit(NO_PIECES, source_index)
-    target_bb = _set_bit(NO_PIECES, target_index)
-
-    foward_bb = (
-        source_bb >> MOVE_VERTICALLY
-        if _is_player_move_direction_up(is_white)
-        else source_bb << MOVE_VERTICALLY
-    )
-
-    if _index_can_move_diagonally(source_index):
-        forward_left_bb = (foward_bb >> MOVE_LEFT_1) & VALID_MOVE_LEFT_1_BB
-        forward_right_bb = (foward_bb << MOVE_RIGHT_1) & VALID_MOVE_RIGHT_1_BB
-
-        # Check target move is a diagonal move by one
-        if (forward_left_bb | forward_right_bb) & target_bb:
-            return True
-
-    # Check target move is a move forward by one
-    if foward_bb & target_bb:
-        return True
-
-    # Check if the target move is a jump move
-    target_source_difference = abs(target_index - source_index)
-
-    # Check if the target move is a jump move by two
-    if target_source_difference not in [10, 2] + (
-        [12, 8] if _index_can_move_diagonally(source_index) else []
-    ):
-        return False
-
-    target_source_mid_index = target_source_difference >> 1
-    target_source_mid_bb = _set_bit(NO_PIECES, target_source_mid_index)
-
-    # Check if there is an enemy piece in between the source and target
-    # if not, then the move is not a jump move and is illegal
-    if not (target_source_mid_bb & enemy_bb):
-        return False
-
-    source_x_coordinate = source_index % BOARD_SIZE_X
-    match (source_x_coordinate):
-        case 0:
-            return target_bb & 0x39CE738
-        case 1:
-            return target_bb & 0xE739CE
-        case 2:
-            return target_bb & 0x1EF7BDE
-        case 3:
-            return target_bb & 0x3FFFFFE
-        case 4:
-            return target_bb & 0x3DEF7BC
-        case _:
-            # This should never happen
-            return False
+    return m in legal_moves(b)  # TODO: optimize
 
 
 def legal_moves(b: Board) -> list[Move]:
@@ -184,7 +118,7 @@ def legal_moves(b: Board) -> list[Move]:
             source_bb >> MOVE_VERTICALLY
             if is_player_move_direction_up
             else source_bb << MOVE_VERTICALLY
-        )
+        ) & VALID_MOVE_BB
 
         if forward_bb and not forward_bb & common_bb:
             moves.append(
@@ -217,22 +151,34 @@ def legal_moves(b: Board) -> list[Move]:
                     )
                 )
 
-        for i in [12, 10, 8, 2]:
+        for i, add_bb, rem_bb in zip(
+            [12, 10, 8, 2],
+            [
+                VALID_MOVE_RIGHT_2_BB,
+                VALID_MOVE_BB,
+                VALID_MOVE_LEFT_2_BB,
+                VALID_MOVE_RIGHT_2_BB,
+            ],
+            [
+                VALID_MOVE_LEFT_2_BB,
+                VALID_MOVE_BB,
+                VALID_MOVE_RIGHT_2_BB,
+                VALID_MOVE_LEFT_2_BB,
+            ],
+        ):
             target_index = source_index + i
-            target_bb = _set_bit(NO_PIECES, target_index)
+            target_bb = source_bb << i
 
-            if target_bb & VALID_MOVE_BB and not target_bb & common_bb:
+            if target_bb & add_bb and not target_bb & common_bb:
                 target_source_mid_bb = _set_bit(NO_PIECES, source_index + (i >> 1))
 
                 if target_source_mid_bb & enemy_bb:
                     moves.append(make_move(source_index, target_index))
 
             negative_target_index = source_index - i
-            negative_target_bb = _set_bit(NO_PIECES, source_index << i)
-            if (
-                negative_target_bb & VALID_MOVE_BB
-                and not negative_target_bb & common_bb
-            ):
+            negative_target_bb = source_bb >> i
+
+            if negative_target_bb & rem_bb and not negative_target_bb & common_bb:
                 target_source_mid_bb = _set_bit(NO_PIECES, source_index - (i >> 1))
                 if target_source_mid_bb & enemy_bb:
                     moves.append(make_move(source_index, negative_target_index))
@@ -259,7 +205,9 @@ def move(m: Move, b: Board) -> None:
 
     if not 4 <= target_source_difference <= 6:
         # Jump move
-        target_source_mid_index = source_index + (target_source_difference >> 1)
+        target_source_mid_index = (
+            source_index if source_index < target_index else target_index
+        ) + (target_source_difference >> 1)
         enemy_bb = _clear_bit(enemy_bb, target_source_mid_index)
 
     if is_white:
