3f8ba1f24b91d88fcce24d0fc025dd3f8907b0d0
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2023-11-09 15:08:45 +0100

    semi working

--- board_bitboard.py
+++ board_bitboard.py
@@ -29,8 +29,8 @@ CAN_MOVE_DIAGONALLY = 0x1  # 1 for odd indices, 0 for even indices
 class Board:
     """A board for the game of alquerque."""
 
-    white_pieces: int
-    black_pieces: int
+    white_bb: int
+    black_bb: int
     is_white_turn: bool
 
 
@@ -56,13 +56,13 @@ def _pieces(bb: int) -> list[int]:
 
 def _win(b: Board) -> bool:
     """Return True if the game is won on board b, otherwise False."""
-    return b.white_pieces == NO_PIECES or b.black_pieces == NO_PIECES
+    return b.white_bb == NO_PIECES or b.black_bb == NO_PIECES
 
 
 def _tie(b: Board) -> bool:
     """Return True if the game is tied on board b, otherwise False.
     Precondition: not _win(b)"""
-    return False
+    return False  # TODO
 
 
 def _index_can_move_diagonally(index: int) -> bool:
@@ -87,19 +87,19 @@ def white_plays(b: Board) -> bool:
 
 def white(b: Board) -> list[int]:
     """Return a list of the white pieces indices on the board."""
-    return _pieces(b.white_pieces)
+    return _pieces(b.white_bb)
 
 
 def black(b: Board) -> list[int]:
     """Return a list of the black pieces indices on the board."""
-    return _pieces(b.black_pieces)
+    return _pieces(b.black_bb)
 
 
 def is_legal(m: Move, b: Board) -> bool:
     """Return True if move m is legal on board b, otherwise False."""
     is_white = white_plays(b)
-    ally_bb = b.white_pieces if is_white else b.black_pieces
-    enemy_bb = b.black_pieces if is_white else b.white_pieces
+    ally_bb = b.white_bb if is_white else b.black_bb
+    enemy_bb = b.black_bb if is_white else b.white_bb
     source_index = source(m)
 
     # Check if the source piece is empty
@@ -170,19 +170,113 @@ def is_legal(m: Move, b: Board) -> bool:
 
 def legal_moves(b: Board) -> list[Move]:
     """Return a list of legal moves on board b for the player whose turn it is to play."""
-    raise NotImplementedError
+    moves = []
+    common_bb = b.white_bb | b.black_bb
+    enemy_bb = b.black_bb if white_plays(b) else b.white_bb
+    is_white = white_plays(b)
+    pieces_indices = white(b) if is_white else black(b)
+    is_player_move_direction_up = _is_player_move_direction_up(is_white)
+    move_direction_multiplier = -1 if is_player_move_direction_up else 1
+
+    for source_index in pieces_indices:
+        source_bb = _set_bit(NO_PIECES, source_index)
+        forward_bb = (
+            source_bb >> MOVE_VERTICALLY
+            if is_player_move_direction_up
+            else source_bb << MOVE_VERTICALLY
+        )
+
+        if forward_bb and not forward_bb & common_bb:
+            moves.append(
+                make_move(
+                    source_index,
+                    source_index + (MOVE_VERTICALLY * move_direction_multiplier),
+                )
+            )
+
+        if _index_can_move_diagonally(source_index):
+            forward_left_bb = (forward_bb >> MOVE_LEFT_1) & VALID_MOVE_LEFT_1_BB
+            forward_right_bb = (forward_bb << MOVE_RIGHT_1) & VALID_MOVE_RIGHT_1_BB
+
+            if forward_left_bb and not forward_left_bb & common_bb:
+                moves.append(
+                    make_move(
+                        source_index,
+                        source_index
+                        + (MOVE_VERTICALLY * move_direction_multiplier)
+                        - 1,
+                    )
+                )
+            if forward_right_bb and not forward_right_bb & common_bb:
+                moves.append(
+                    make_move(
+                        source_index,
+                        source_index
+                        + (MOVE_VERTICALLY * move_direction_multiplier)
+                        + 1,
+                    )
+                )
+
+        for i in [12, 10, 8, 2]:
+            target_index = source_index + i
+            target_bb = _set_bit(NO_PIECES, target_index)
+
+            if target_bb & VALID_MOVE_BB and not target_bb & common_bb:
+                target_source_mid_bb = _set_bit(NO_PIECES, source_index + (i >> 1))
+
+                if target_source_mid_bb & enemy_bb:
+                    moves.append(make_move(source_index, target_index))
+
+            negative_target_index = source_index - i
+            negative_target_bb = _set_bit(NO_PIECES, source_index << i)
+            if (
+                negative_target_bb & VALID_MOVE_BB
+                and not negative_target_bb & common_bb
+            ):
+                target_source_mid_bb = _set_bit(NO_PIECES, source_index - (i >> 1))
+                if target_source_mid_bb & enemy_bb:
+                    moves.append(make_move(source_index, negative_target_index))
+
+    return moves
 
 
 def move(m: Move, b: Board) -> None:
-    """Make move m on board b."""
-    raise NotImplementedError
+    """Make move m on board b.
+    Precondition: is_legal(m, b)"""
+    is_white = white_plays(b)
+    source_index = source(m)
+    target_index = target(m)
+    ally_bb = b.white_bb if is_white else b.black_bb
+    enemy_bb = b.black_bb if is_white else b.white_bb
+
+    # Remove the source piece from the board
+    ally_bb = _clear_bit(ally_bb, source_index)
+
+    # Add the target piece to the board
+    ally_bb = _set_bit(ally_bb, target_index)
+
+    target_source_difference = abs(target_index - source_index)
+
+    if not 4 <= target_source_difference <= 6:
+        # Jump move
+        target_source_mid_index = source_index + (target_source_difference >> 1)
+        enemy_bb = _clear_bit(enemy_bb, target_source_mid_index)
+
+    if is_white:
+        b.white_bb = ally_bb
+        b.black_bb = enemy_bb
+    else:
+        b.white_bb = enemy_bb
+        b.black_bb = ally_bb
+
+    b.is_white_turn = not is_white
 
 
 def is_game_over(b: Board) -> bool:
     """Return True if the game is over on board b, otherwise False."""
-    raise NotImplementedError
+    return _win(b) or _tie(b)
 
 
 def copy(b: Board) -> Board:
     """Return a copy of board b."""
-    return Board(b.white_pieces, b.black_pieces, b.is_white_turn)
+    return Board(b.white_bb, b.black_bb, b.is_white_turn)
